bb32505cac11d1970c9fbbdc3bc7059150cfdc8a
AdarshBajracharya <112367556+AdarshBajracharya@users.noreply.github.com>
2022-09-12 10:06:18 +0545

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,22 @@
+age = int(input("please provide your age : "))
+
+#conditional statement
+#if 
+"""
+print("Code started....")
+if age>18:
+    print(f"You are eligible to vote{age}")
+else:
+    print("You are not eligible to vote") 
+
+"""
+"""
+if 0<age<=10:
+    print("Pay 100 for the ticket")
+elif 10<age<=20:
+    print("Pay 200 for the ticket")
+else:
+    print("Pay 300 for the ticket")
+"""
+
+    
